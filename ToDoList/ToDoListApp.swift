98f86d1a4cf400a14dad4428e586d5f5d46a171a
Oleksii Pysarenko <pysarole@gmail.com>
2025-01-04 12:40:22 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Oleksii Pysarenko on 04.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
