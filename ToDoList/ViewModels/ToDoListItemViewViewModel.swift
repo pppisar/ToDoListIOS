23fc8fc885c948d1b11406e2ec66eecb1cb6b9e4
Oleksii Pysarenko <pysarole@gmail.com>
2025-01-05 10:05:02 +0100

    feat: Profile page and logout functionality

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Oleksii Pysarenko on 04.01.2025.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 // ViewModel for single to do list item view (each row in items list)
@@ -12,6 +14,20 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        // Create a copy because item is a contant value
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
 }
